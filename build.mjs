bb06aac46dae5e209ddc3147ba17a7387f9f6998
Redskull-127 <meerraja17@gmail.com>
2024-02-12 02:10:45 +0530

    Fixed: Node esm script bug v0.0.2-stable

--- build.mjs
+++ build.mjs
@@ -2,7 +2,11 @@ import dts from 'bun-plugin-dts'
 
 await Bun.build({
   entrypoints: ['./src/index.ts'],
+  format: 'esm',
   outdir: './dist',
+  splitting: false,
+  naming: "index.mjs",
   minify: true,
-  plugins: [dts()]
+  plugins: [dts()],
+  target: 'node',
 })
